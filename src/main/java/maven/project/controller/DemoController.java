b9eaba5dd3c7d41c3f79136f5775485868ff5935
AiRusu <aida.rusu93@gmail.com>
2020-09-29 23:22:23 +0300

    adding WebAppInitializer, WebConfig and DemoController

---
+++ src/main/java/maven/project/controller/DemoController.java
@@ -0,0 +1,22 @@
+package maven.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    //http://localhost:9980/todo-list/hello
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    //http://localhost:9980/todo-list/welcome
+    @GetMapping("welcome")
+    public String welcome(){
+        return "welcome";
+    }
+}
