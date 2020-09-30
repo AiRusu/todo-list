93259fc652edd53a98536a3e83d18e7476e5643b
AiRusu <aida.rusu93@gmail.com>
2020-09-30 20:11:53 +0300

    adding model and service packages and some of the containing classes

---
+++ src/main/java/maven/project/service/DemoService.java
@@ -0,0 +1,8 @@
+package maven.project.service;
+
+public interface DemoService {
+
+    String getHelloMessage(String user);
+
+    String getWelcomeMessage();
+}
---
+++ src/main/java/maven/project/util/Mappings.java
@@ -0,0 +1,9 @@
+package maven.project.util;
+
+public final class Mappings {
+
+    // == constants ==
+    private Mappings() {
+
+    }
+}
